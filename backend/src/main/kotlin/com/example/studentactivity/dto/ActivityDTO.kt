1d39c1876ecff0a28c142b0f62564478e9a8daf8
Avel Kristian Ladano <avelkristian321@gmail.com>
2025-12-10 17:25:07 +0800

    fix: add jakarta constraints for description

--- backend/src/main/kotlin/com/example/studentactivity/dto/ActivityDTO.kt
+++ backend/src/main/kotlin/com/example/studentactivity/dto/ActivityDTO.kt
@@ -1,10 +1,13 @@
 package com.example.studentactivity.dto
 
 import com.example.studentactivity.entity.enums.ActivityType
+import jakarta.validation.constraints.NotBlank
 import java.time.Instant
 
 data class ActivityRequest(
     val type: ActivityType,
+
+    @field:NotBlank(message="Description is required")
     val description: String
 )
 
